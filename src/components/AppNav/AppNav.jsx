1ebc1320e25db8532f26b12ed361b2f49c86f354
bora <nhatbaoxxd@gmail.com>
2023-12-05 05:37:56 +0700

    Implement nav for app layout (WIP)

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -1,3 +1,18 @@
+import { NavLink } from 'react-router-dom'
+import styles from './AppNav.module.css'
+import { PATH } from '@/constants/path'
+
 export default function AppNav() {
-  return <div>AppNav</div>
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to={PATH.APP.CITIES}>Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to={PATH.APP.COUNTRIES}>Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
 }
