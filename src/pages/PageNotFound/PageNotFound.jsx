ad8516a66843b06d83615ad0e38f6145bfef8c97
bora <nhatbaoxxd@gmail.com>
2023-12-04 18:14:24 +0700

    Implement page not found

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom'
+import { PATH } from '@/constants/path'
+import styles from './PageNotFound.module.css'
+
 export default function PageNotFound() {
-  return <div>PageNotFound</div>
+  return (
+    <section className={styles.pageNotFound}>
+      <section>
+        <h1>404</h1>
+        <h2>Sorry, page not found 😢</h2>
+        <Link to={PATH.HOMEPAGE} className="cta">
+          Go back home
+        </Link>
+      </section>
+    </section>
+  )
 }
