44590c3e6d2802c9be47a8d1fc529b476c5234ec
bora <nhatbaoxxd@gmail.com>
2023-12-06 09:18:27 +0700

    Reading and setting a query string

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,4 +1,4 @@
-import { Link, generatePath } from 'react-router-dom'
+import { Link, createSearchParams, generatePath } from 'react-router-dom'
 
 import { PATH } from '@/constants/path'
 import { formatDate } from '@/utils/common'
@@ -6,13 +6,14 @@ import { Flag } from '@/components/Flag'
 import styles from './CityItem.module.css'
 
 export default function CityItem({ city }) {
-  const { id, cityName, countryCode, country, date } = city
+  const { id, cityName, countryCode, country, date, position } = city
 
   const cityPath = generatePath(PATH.APP.CITY, { id })
+  const citySearchParams = createSearchParams({ lat: position.lat, lng: position.lng }).toString()
 
   return (
     <li>
-      <Link to={cityPath} className={styles.cityItem}>
+      <Link to={{ pathname: cityPath, search: citySearchParams }} className={styles.cityItem}>
         <div className={styles.flag}>
           <Flag countryCode={countryCode} countryName={country} />
         </div>
