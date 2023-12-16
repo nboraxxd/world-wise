f181b164427fe50d388c9e327a51c358a0afbd65
bora <nhatbaoxxd@gmail.com>
2023-12-16 17:43:34 +0700

    Setting Map Position With Geolocation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet'
 
 import { useCities } from '@/contexts/cities.context'
+import { useGeolocation } from '@/hooks/useGeolocation'
 import { CenterPosition } from '@/components/CenterPosition'
-import { Flag } from '@/components/Flag'
-import styles from './Map.module.css'
 import { DetectClick } from '@/components/DetectClick'
+import { Flag } from '@/components/Flag'
+import { Button } from '@/components/Button'
+import styles from './Map.module.css'
 
 export default function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0])
-
-  const { cities } = useCities()
-
   const [searchParams] = useSearchParams()
+  const { cities } = useCities()
+  const { getPosition, isLoading: isLoadingPosition, position: geolocationPosition } = useGeolocation()
+
   const mapLat = searchParams.get('lat')
   const mapLng = searchParams.get('lng')
 
@@ -23,13 +25,24 @@ export default function Map() {
     }
   }, [mapLat, mapLng])
 
+  useEffect(() => {
+    if (geolocationPosition) {
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    }
+  }, [geolocationPosition])
+
   return (
     <div className={styles.mapContainer}>
-      <MapContainer center={mapPosition} zoom={6} className={styles.map}>
+      <Button type="position" onClick={getPosition}>
+        {isLoadingPosition ? 'Loading...' : 'Use your position'}
+      </Button>
+
+      <MapContainer center={mapPosition} zoom={6} zoomControl={false} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+        <ZoomControl position="topright" />
         {cities.map((city) => (
           <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
             <Popup>
