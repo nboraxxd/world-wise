5e3059ac1d36e7228dee4957d3201f6383b41036
bora <nhatbaoxxd@gmail.com>
2023-12-16 14:50:22 +0700

    Including a Map With the Leaflet Library

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,30 @@
+import { useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import { PATH } from '@/constants/path'
 import styles from './Map.module.css'
 
 export default function Map() {
+  const [mapPosition, setMapPosition] = useState([40, 0])
+  const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
-  const navigate = useNavigate()
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate(PATH.APP.FORM)}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
+    <div className={styles.mapContainer}>
+      <MapContainer center={mapPosition} zoom={13} className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
