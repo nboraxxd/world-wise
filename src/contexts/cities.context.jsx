715c665356322f0c23f40b98f4836102bf495b31
bora <nhatbaoxxd@gmail.com>
2023-12-19 11:32:12 +0700

    Advanced state management system useReducer

--- src/contexts/cities.context.jsx
+++ src/contexts/cities.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useMemo, useReducer } from 'react'
 import { toast } from 'sonner'
 
 import useFetch from '@/hooks/useFetch'
@@ -6,33 +6,63 @@ import { citiesService } from '@/services/cities.service'
 
 const CitiesContext = createContext()
 
+const initialState = {
+  cities: [],
+  status: 'idle',
+  currentCity: {},
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'cities/pending':
+      return { ...state, status: 'pending' }
+
+    case 'cities/fetched':
+      return { ...state, cities: action.payload }
+
+    case 'city/created':
+      return { ...state, cities: [...state.cities, action.payload], currentCity: action.payload, status: 'successful' }
+
+    case 'city/current':
+      return { ...state, currentCity: action.payload }
+
+    case 'city/deleted':
+      return {
+        ...state,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+        status: 'successful',
+      }
+
+    case 'cities/rejected':
+      return { ...state, status: 'rejected' }
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
 export default function CitiesProvider({ children }) {
-  const [currentCity, setCurrentCity] = useState({})
-  const [cities, setCities] = useState([])
-  const [status, setStatus] = useState('idle')
-  const [error, setError] = useState('')
+  const [{ cities, status, currentCity }, dispatch] = useReducer(reducer, initialState)
 
   const { data: _cities, status: fetchStatus, error: fetchError } = useFetch(citiesService.getCities)
 
   useEffect(() => {
     if (_cities) {
-      setCities(_cities)
+      dispatch({ type: 'cities/fetched', payload: _cities })
     }
   }, [_cities])
 
   async function createCity(newCity) {
     try {
-      setStatus('pending')
+      dispatch({ type: 'cities/pending' })
 
       const res = await citiesService.createCity(newCity)
 
-      setCities((prevCities) => [...prevCities, res])
-      setStatus('successful')
+      dispatch({ type: 'city/created', payload: res })
     } catch (err) {
-      setStatus('rejected')
       toast.error(err.message || 'Something went wrong')
-      setError(err.message || 'Something went wrong')
-
+      dispatch({ type: 'cities/rejected', payload: err })
       throw err
     }
   }
@@ -41,15 +71,14 @@ export default function CitiesProvider({ children }) {
     if (!cityId) return
 
     try {
-      setStatus('pending')
+      dispatch({ type: 'cities/pending' })
 
       await citiesService.deleteCity(cityId)
-      setCities((prevCities) => prevCities.filter((city) => city.id !== cityId))
 
-      setStatus('successful')
+      dispatch({ type: 'city/deleted', payload: cityId })
     } catch (err) {
-      setStatus('rejected')
       toast.error(err.message || 'Something went wrong')
+      dispatch({ type: 'cities/rejected', payload: err })
       throw err
     }
   }
@@ -58,15 +87,13 @@ export default function CitiesProvider({ children }) {
     <CitiesContext.Provider
       value={{
         cities,
-        setCities,
         fetchStatus,
         fetchError,
         currentCity,
-        setCurrentCity,
+        setCurrentCity: useMemo(() => (city) => dispatch({ type: 'city/current', payload: city }), []),
         createCity,
         deleteCity,
         cityStatus: status,
-        cityError: error,
       }}
     >
       {children}
